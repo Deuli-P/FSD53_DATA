7f09b623dc5d840a5f60e1f28368f8953336a35b
Antoniutti Pierre <pierre.antoniutti@gmail.com>
2024-11-08 14:39:42 +0100

    /user

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ connection.connect((err) => {
     }); 
 });
 
-app.get('/',(req,res)=> {
+app.get('/user',(req,res)=> {
     try{
         connection.query(
             'SELECT u.username AS "photographe", p.title AS "photo" FROM user AS u JOIN photo AS p ON p.user_id = u.id; ',
